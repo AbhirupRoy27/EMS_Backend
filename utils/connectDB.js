bdc5c89f6d2924c9373395e7226886524adea625
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-11-06 20:31:10 +0530

    feat: implement MongoDB connection utility with Mongoose

---
+++ utils/connectDB.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+let isConnected = false
+
+export default async function connectDB() {
+  if (isConnected) return
+  try {
+    await mongoose.connect(process.env.DB_URI)
+    isConnected = true
+    if (mongoose.connection.readyState === 1) {
+      console.log(
+        '✅ Mongoose is connected, DB_Name:',
+        mongoose.connection.name
+      )
+    } else {
+      console.log('⚠️ Mongoose is not connected')
+    }
+  } catch (err) {
+    console.error('failed to connect to MongoDB', err)
+  }
+}
